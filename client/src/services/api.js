ab7e72ce7b42636abc0a3c3331fee89f53443d8c
Ronald Joseph <ronaldjoseph439@gmail.com>
2026-03-14 17:46:56 +0530

    updated api with deployed links

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://yapper-hub-a-chat-app.vercel.app/",
     withCredentials: true
 })
 
