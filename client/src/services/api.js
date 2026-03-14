061f95be49a76b11dd6b1ac338e04cb55f01fd23
Ronald Joseph <ronaldjoseph439@gmail.com>
2026-03-14 18:56:06 +0530

    api fix

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://yapper-hub-a-chat-app.vercel.app/",
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
     withCredentials: true
 })
 
